d335f3ed0f4337572206a21ef112dd252c40a7ec
infinity-atom <Leonard.jj@outlook.com>
2023-08-04 17:53:06 +1000

    Add other options to ArgumentHandler

--- src/main/java/org/plasmascripts/PlasmaScripts/ArgumentHandler.java
+++ src/main/java/org/plasmascripts/PlasmaScripts/ArgumentHandler.java
@@ -14,4 +14,31 @@ public class ArgumentHandler {
         return hasArg;
     }
 
+    public static String argValueOption(String argOption) {
+        /*
+
+            CODE NOT CHECKED YET
+
+         */
+        boolean hasArg = false;
+        String valueOut = "";
+        int currentArgIndex = 0;
+        for(String arg : argList) {
+            if(arg.equalsIgnoreCase(argOption)) {
+                hasArg = true;
+                break;
+            }
+            currentArgIndex += 1;
+        }
+        if(hasArg) {
+            if(currentArgIndex == argList.length) {
+                valueOut = "";
+            } else {
+                valueOut = argList[currentArgIndex + 1];
+            }
+        }
+
+        return valueOut;
+    }
+
 }
--- src/main/java/org/plasmascripts/PlasmaScripts/Main.java
+++ src/main/java/org/plasmascripts/PlasmaScripts/Main.java
@@ -5,10 +5,6 @@ public class Main {
         // add args to handler
         ArgumentHandler.argList = args;
 
-        if(ArgumentHandler.argSingleOptionPresent("-debug")) {
-            for(String i : args) {
-                System.out.println(i);
-            }
-        }
+
     }
 }
